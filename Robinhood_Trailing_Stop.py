263aac3eac114d4a675b30006a9f3c8ff8b098d6
Stephen <stephen@Stephens-MacBook-Air.local>
2018-08-17 06:10:36 -0700

    lots of things on trailing stop

--- Robinhood_Trailing_Stop.py
+++ Robinhood_Trailing_Stop.py
@@ -3,6 +3,7 @@ import os
 import sys
 import datetime
 import logging
+import time
 
 from Robinhood import Robinhood
 
@@ -30,7 +31,7 @@ class Robinhood_Trailing_Stop:
         self.trader.login(username = self.username, password = self.password)
 
         # Start the program
-        self._main()
+        self.main()
 
     def _valid_percent(self, n):
         #TODO: Add description
@@ -40,43 +41,53 @@ class Robinhood_Trailing_Stop:
             return False
         return False
 
-    def _main(self):
+    def main(self):
         #TODO: add description
-        last_cancel_check = datetime.datetime.now()
-        last_sale_check = datetime.datetime.now()
+        start_time = datetime.datetime.now()
 
-        #TODO: Move these things to functions so that they can be called at the start of _main and on a schedule.
 
         while True:
-            if (datetime.datetime.now()-last_cancel_check).total_seconds() > 60 * 60:
+
+            # Check if day is over
+            time_dif =(datetime.datetime.now() - start_time).total_seconds()
+            if time_dif > 60 * 60 * 7.1:
+                break
 
             # Cancel existing sell orders
-            logging.info('Cancelling any open sell orders.')
-            n = self.trader.cancel_all_sell_orders()
-            logging.info('There were {} sell orders cancelled'.format(n))
+            self.cancel_all_sells()
+            time.sleep(1)
 
             # Check Stock
-            self.stock_list = [x for x in self.trader.nonzero_positions_held()]
-            n = len(self.stock_list)
-            logging.info('There are {} stocks held currently.'.format(n))
-            for stock in self.stock_list:
-                self.check_for_sale(stock)
+            self.check_for_stocks()
+            time.sleep(60*2)
+    
+    def check_for_stocks(self):
+        #TODO: Add Description
+
+        self.stock_list = [x for x in self.trader.nonzero_positions_held()]
+        n = len(self.stock_list)
+        logging.info('There are {} stocks held currently.'.format(n))
+
+        for stock in self.stock_list:
+            self.check_for_sale(stock)
 
     def check_for_sale(self, stock):
         #TODO: Add Description
         # Checks the stock bid price against the high to see if it has surpassed the stop price
         
+        # Get basic stock info needed
         instrument = stock['instrument']
         quantity = int(float(stock['quantity']))
         symbol = self.trader.instrument_results(instrument)['symbol']
-        
         assert symbol, "You must provide a symbol."
+        
+        # Get Stock Price Info
         hi = float(self.trader.high_52_weeks(symbol))
-        last = float(self.trader.high_52_weeks(symbol))
-
+        last = float(self.trader.quote(symbol)['last_trade_price']) # If this is anything other than "Last", such as bid/ask, then need to check that price isn't $0 during after-hours
         stop = (1-self.tp) * hi
-        limit = (1 - self.tp - self.lp) * hi
+        limit = (1 - self.lp) * last
 
+        # IF the price has fallen below the threshold
         if last <= stop:
             # Place Order
             limit = round(limit,2)
@@ -92,6 +103,8 @@ class Robinhood_Trailing_Stop:
 
     def cancel_all_sells(self):
         #TODO: add description
+
+        logging.info('Cancelling any open sell orders.')
 
         try:
             c = self.trader.cancel_all_sell_orders()
